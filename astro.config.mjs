9a5c88832a49d699b132c8e240d4d82b08a2f900
JRamonCarralero <grukduz@gmail.com>
2026-04-27 23:00:54 +0200

    feat: :sparkles: Github Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/AdoptaUnJuniorPlatform.io',
+  base: '/devfest-aranjuez-2026',
   vite: {
     plugins: [tailwindcss()],
   },
