a019939018d42930697abde09d24fe2bba3504e3
José Ramón Carralero <grukduz@gmail.com>
2026-05-05 21:17:44 +0200

    Change base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://devfest2026.gdg-aranjuez.com',
-  base: '/devfest-aranjuez-2026',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
