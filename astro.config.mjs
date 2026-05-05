7299460d5d6e63f581620a55c9f163b650167e4e
José Ramón Carralero <grukduz@gmail.com>
2026-05-05 21:14:39 +0200

    Remove trailing slash from site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devfest2026.gdg-aranjuez.com/',
+  site: 'https://devfest2026.gdg-aranjuez.com',
   base: '/devfest-aranjuez-2026',
   vite: {
     plugins: [tailwindcss()],
